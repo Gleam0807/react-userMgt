51b2aaf101882c2541a43506ae9f84a6769a2d67
Gleam0807 <teemocscsg@naver.com>
2022-02-27 17:29:14 +0900

    0.0.2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="darkblue-background">
-      <header>
-        <img src={logo} alt="logo" />
-        <h2>Let's Go</h2>
-      </header>
-    </div>
-  );
+const customers = [
+{
+  'id': 1,
+  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+  'name': '라이언',
+  'birthday': '170101',
+  'gender': '남자',
+  'job': '마스코트'
+},
+{
+  'id': 2,
+  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+  'name': '라이언',
+  'birthday': '150101',
+  'gender': '남자',
+  'job': '마스코트'
+},
+{
+  'id': 3,
+  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+  'name': '라이언',
+  'birthday': '190101',
+  'gender': '남자',
+  'job': '마스코트'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
+
