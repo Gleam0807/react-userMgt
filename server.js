89341aab8199392cab84cc0c9a2c2d0c23729013
Gleam0807 <teemocscsg@naver.com>
2022-03-01 16:43:27 +0900

    react node api 처리

--- server.js
+++ server.js
@@ -5,8 +5,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    setTimeout(function(){
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+            'name': '라이언',
+            'birthday': '170101',
+            'gender': '남자',
+            'job': '마스코트'
+        },
+        {
+            'id': 2,
+            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+            'name': '라이언',
+            'birthday': '150101',
+            'gender': '남자',
+            'job': '마스코트'
+        },
+        {
+            'id': 3,
+            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
+            'name': '라이언',
+            'birthday': '190101',
+            'gender': '남자',
+            'job': '마스코트'
+        }
+    ]);
+    },2000);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,38 +18,42 @@ const styles = theme => ({
   },
   table: {
   minWidth: 1080
-  }
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
+    }
   });
-  
-
-const customers = [
-{
-  'id': 1,
-  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-  'name': '라이언',
-  'birthday': '170101',
-  'gender': '남자',
-  'job': '마스코트'
-},
-{
-  'id': 2,
-  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-  'name': '라이언',
-  'birthday': '150101',
-  'gender': '남자',
-  'job': '마스코트'
-},
-{
-  'id': 3,
-  'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-  'name': '라이언',
-  'birthday': '190101',
-  'gender': '남자',
-  'job': '마스코트'
-}
-]
 
 class App extends Component {
+  
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,9 +70,15 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ? this.state.customers.map(c => {
               return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            })}
+            }): 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className = {classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
