cc43df2235ae720e7a83118c8d7f5d29b7ce7034
Gleam0807 <teemocscsg@naver.com>
2022-03-02 21:43:53 +0900

    mysql 연동 및 글 post 추가

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,35 +6,48 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req, res) => {
-    setTimeout(function(){
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-            'name': '라이언',
-            'birthday': '170101',
-            'gender': '남자',
-            'job': '마스코트'
-        },
-        {
-            'id': 2,
-            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-            'name': '라이언',
-            'birthday': '150101',
-            'gender': '남자',
-            'job': '마스코트'
-        },
-        {
-            'id': 3,
-            'image': 'https://item.kakaocdn.net/do/d84248170c2c52303db27306a00fb861effd194bae87d73dd00522794070855d',
-            'name': '라이언',
-            'birthday': '190101',
-            'gender': '남자',
-            'job': '마스코트'
-        }
-    ]);
-    },2000);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
 });
+
+connection.connect();
+
+
+app.get('/api/customers', (req, res) => {
+        connection.query(
+            'SELECT * FROM CUSTOMER',
+            (err, rows, fields) => {
+                res.send(rows);
+            }
+        )
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null,?,?,?,?,?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params, 
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
